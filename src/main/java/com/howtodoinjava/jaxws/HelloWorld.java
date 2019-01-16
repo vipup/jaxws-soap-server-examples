f1b6d5c0c3d55a1880c01351974ef0986ff1e105
Bic Ed <devan.oppenheimer@uniposta.de>
2019-01-16 15:31:31 +0100

    it MUST NOT be annotated with @SOAPBinding,

--- src/main/java/com/howtodoinjava/jaxws/HelloWorld.java
+++ src/main/java/com/howtodoinjava/jaxws/HelloWorld.java
@@ -3,15 +3,12 @@ package com.howtodoinjava.jaxws;
 import java.net.URISyntaxException;
 
 import javax.jws.WebService;
-import javax.jws.soap.SOAPBinding;
-import javax.jws.soap.SOAPBinding.Style;
-import javax.jws.soap.SOAPBinding.Use;
+ 
 
 import com.howtodoinjava.soap.Employee;
 import com.howtodoinjava.soap.Employees;
 //Service Endpoint Interface
 @WebService
-@SOAPBinding(style = Style.RPC, use=Use.ENCODED)
 public interface HelloWorld {
 
 	Employee deleteEmployeeById(Integer id);

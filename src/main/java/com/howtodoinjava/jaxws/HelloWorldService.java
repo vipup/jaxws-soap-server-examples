1c7881a842721b7cd33a7637478d53cbd4f53588
Bic Ed <devan.oppenheimer@uniposta.de>
2019-01-16 15:33:16 +0100

    it MUST NOT be annotated with @SOAPBinding

--- src/main/java/com/howtodoinjava/jaxws/HelloWorldService.java
+++ src/main/java/com/howtodoinjava/jaxws/HelloWorldService.java
@@ -5,14 +5,12 @@ import java.util.ArrayList;
 
 import javax.jws.WebMethod;
 import javax.jws.WebService;
-import javax.jws.soap.SOAPBinding;
-import javax.jws.soap.SOAPBinding.Style;
-import javax.jws.soap.SOAPBinding.Use;
+ 
 
 import com.howtodoinjava.soap.Employee;
 import com.howtodoinjava.soap.Employees;
   
-@SOAPBinding(style = Style.DOCUMENT, use=Use.LITERAL)
+ 
 @WebService(endpointInterface = "com.howtodoinjava.jaxws.HelloWorld")
 public class HelloWorldService implements HelloWorld{
 	

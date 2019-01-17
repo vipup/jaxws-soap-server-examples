26ec010df04432432785c950d7a95e861f706e57
Bic Ed <devan.oppenheimer@uniposta.de>
2019-01-17 08:00:18 +0100

    -----

--- src/main/java/com/howtodoinjava/jaxws/HelloWorldService.java
+++ src/main/java/com/howtodoinjava/jaxws/HelloWorldService.java
@@ -1,14 +1,8 @@
 package com.howtodoinjava.jaxws;
-  
-import java.net.URISyntaxException;
-import java.util.ArrayList;
-
-import javax.jws.WebMethod;
+   
 import javax.jws.WebService;
  
-
-import com.howtodoinjava.soap.Employee;
-import com.howtodoinjava.soap.Employees;
+ 
   
  
 @WebService(endpointInterface = "com.howtodoinjava.jaxws.HelloWorld")
@@ -17,9 +11,8 @@ public class HelloWorldService implements HelloWorld{
 	@Override
     public String getMsg(String msg)
     {
-        String output = "Message requested : " + msg;
-        //Simply return the parameter passed as message
-        return output;
+		return   "Message requested : " + msg; 
+        
     }
     
 }

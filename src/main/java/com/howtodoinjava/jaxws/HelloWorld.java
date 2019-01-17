8f4f14bf2f6cee890800f126a5f4668295f5a1eb
Bic Ed <devan.oppenheimer@uniposta.de>
2019-01-17 07:59:08 +0100

    -----

--- src/main/java/com/howtodoinjava/jaxws/HelloWorld.java
+++ src/main/java/com/howtodoinjava/jaxws/HelloWorld.java
@@ -1,14 +1,11 @@
 package com.howtodoinjava.jaxws;
-
-import java.net.URISyntaxException;
+ 
 
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 import javax.jws.soap.SOAPBinding.Style;
-
-import com.howtodoinjava.soap.Employee;
-import com.howtodoinjava.soap.Employees;
+ 
 //Service Endpoint Interface
 @WebService
 @SOAPBinding(style = Style.DOCUMENT) 

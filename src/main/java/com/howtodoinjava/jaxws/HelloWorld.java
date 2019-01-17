eac670db54068f1ab8c7143ff9eb8fbedec6854c
Bic Ed <devan.oppenheimer@uniposta.de>
2019-01-17 09:37:51 +0100

    revert_N2

--- src/main/java/com/howtodoinjava/jaxws/HelloWorld.java
+++ src/main/java/com/howtodoinjava/jaxws/HelloWorld.java
@@ -1,17 +1,32 @@
 package com.howtodoinjava.jaxws;
  
 
+import java.net.URISyntaxException;
+
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 import javax.jws.soap.SOAPBinding.Style;
+
+import com.howtodoinjava.soap.Employee;
+import com.howtodoinjava.soap.Employees;
  
 //Service Endpoint Interface
 @WebService
 @SOAPBinding(style = Style.DOCUMENT) 
 public interface HelloWorld {
-
- 
+	@WebMethod
+	Employee deleteEmployeeById(Integer id);
+	@WebMethod
+	Employee updateEmployeeById(Integer id, Employee e);
+	@WebMethod
+	Employee addEmployee(Employee e) throws URISyntaxException;
+	@WebMethod
+	Employee updateEmployeeById(Integer id);
+	@WebMethod
+	Employees getAllEmployees();
+	
+	
 	@WebMethod
 	String getMsg(String msg);
 
--- src/main/java/com/howtodoinjava/jaxws/HelloWorldService.java
+++ src/main/java/com/howtodoinjava/jaxws/HelloWorldService.java
@@ -1,13 +1,56 @@
 package com.howtodoinjava.jaxws;
    
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+
 import javax.jws.WebService;
+
+import com.howtodoinjava.soap.Employee;
+import com.howtodoinjava.soap.Employees;
  
  
   
  
 @WebService(endpointInterface = "com.howtodoinjava.jaxws.HelloWorld")
 public class HelloWorldService implements HelloWorld{
-	 
+	public Employee deleteEmployeeById(Integer id) {
+		return new Employee(1, "Deleted Arnold");
+	}
+
+	@Override
+	public Employee updateEmployeeById(Integer id, Employee e) {
+		return e;
+	}
+
+	@Override
+	public Employee addEmployee(Employee e) throws URISyntaxException {
+		return e;
+	}
+
+	@Override
+	public Employee updateEmployeeById(Integer id) {
+
+		Employee emp = new Employee(id, "Updated Max");
+
+		emp.setId(id);
+		emp.setName("Lokesh Gupta");
+
+		return emp;
+	}
+
+	@Override
+	public Employees getAllEmployees() {
+		Employees list = new Employees();
+		list.setEmployeeList(new ArrayList<Employee>());
+
+		list.getEmployeeList().add(new Employee(1, "Lokesh Gupta"));
+		list.getEmployeeList().add(new Employee(2, "Alex Kolenchiskey"));
+		list.getEmployeeList().add(new Employee(3, "David Kameron"));
+
+		return list;
+	}
+ 
+ 
 	@Override
     public String getMsg(String msg)
     {

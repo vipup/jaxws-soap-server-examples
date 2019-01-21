fba3bc488d703d8c8e0fd89a268cf1a1c908e09f
Bic Ed <devan.oppenheimer@uniposta.de>
2019-01-21 08:50:35 +0100

    warnings--

--- src/main/java/com/howtodoinjava/jaxws/HelloWorld.java
+++ src/main/java/com/howtodoinjava/jaxws/HelloWorld.java
@@ -7,16 +7,20 @@ import javax.jws.WebMethod;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 import javax.jws.soap.SOAPBinding.Style;
+import javax.jws.soap.SOAPBinding.Use;
 
 import com.howtodoinjava.soap.Employee;
 import com.howtodoinjava.soap.Employees;
  
 //Service Endpoint Interface
 @WebService
-@SOAPBinding(style = Style.RPC) 
+@SOAPBinding(use = Use.ENCODED,  style = Style.RPC) 
 public interface HelloWorld {
 	@WebMethod
 	Employee deleteEmployeeById(Integer id);
+	@WebMethod
+	Employee getEmployeeById(Integer id);
+
 	@WebMethod
 	Employee updateEmployeeById(Integer id, Employee e);
 	@WebMethod
